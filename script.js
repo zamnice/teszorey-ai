ca1b2a055b6af9346d3b45f894a3651ee4bc4f89
ZAM ZAM <217864473+zamnice@users.noreply.github.com>
2025-07-25 21:32:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // script.js
 // Ambil API Key dari environment variable
-const apiKey = 'AIzaSyB34InSIBQkm5S0dpHvXmR1aFYu5r2NB_s'
+const apiKey = 'GEMMA_API_KEY'
 
 // Fungsi untuk mengirim pesan ke AI
 async function sendMessage(message) {
-    const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemma-3-1b-it:generateContent?key=to'AIzaSyB34InSIBQkm5S0dpHvXmR1aFYu5r2NB_s';
+    const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemma-3-1b-it:generateContent?key=to' + apiKey;
     const data = {
         contents: [{
             parts: [{ text: message }]
